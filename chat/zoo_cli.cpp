9e1a305d6b5852915ff5859a0e86f9466d132637
ZHOU <zksilence@gmail.com>
2018-01-31 11:12:22 +0800

    add return error

--- chat/zoo_cli.cpp
+++ chat/zoo_cli.cpp
@@ -45,11 +45,11 @@ char* type2String(int state){
 
   return "UNKNOWN_EVENT_TYPE";
 }
-void watcher(zhandle_t *zzh,int type,int state,const char*path,void *watcherCtx){
+int watcher(zhandle_t *zzh,int type,int state,const char*path,int *watcherCtx){
 	std::cout<<"zookeeper connected"<<endl;
 	//std::cout<<"Watcher "<<type2String(type) <<"state ="<<state2String(state);
 }
-void my_string_completion_free_data(int rc, const char *name, const void *data){
+int my_string_completion_free_data(int rc, const char *name, const int *data){
 	std::cout<<(data==0?"null":data)<<": rc = "<<rc<<std::endl;
 	if (!rc) {
        std::cout<<"name = "<<name<<std::endl;
@@ -85,22 +85,25 @@ Zoo_cli::Zoo_cli(char *ip){
  * 初始化节点,即建立brokes,topics,consumers节点目录
  * 系统只运行一次
  * *********/
-void Zoo_cli::init(){
+int Zoo_cli::init(){
 	int rc = zoo_create(zh,"/brokers","",0,&ZOO_OPEN_ACL_UNSAFE,0,
 				0,0);
 	if(rc){
 		std::cout<<"init error :brokers!rc = "<<rc<<endl;
+		return rc;
 	}
 	rc = zoo_create(zh,"/consumers","",0,&ZOO_OPEN_ACL_UNSAFE,0,
 				0,0);
 	if(rc){
 			std::cout<<"init error :consumers! rc="<<rc<<endl;
+			return rc;
 		}
 	rc = zoo_create(zh,"/topics","",0,&ZOO_OPEN_ACL_UNSAFE,0,
 					0,0);
 	if(rc){
 				std::cout<<"init error :topics! rc = "<<rc<<endl;;
 			}
+	return rc;
 }
 Zoo_cli::~Zoo_cli(){
 	zookeeper_close(this->zh);
@@ -109,7 +112,7 @@ Zoo_cli::~Zoo_cli(){
  * br_name 要建立broker节点的name 一般可为ip地址
  * 创建的broker为临时节点,掉线自动删除
  * ************/
-void Zoo_cli::register_broker(char * br_name){
+int Zoo_cli::register_broker(char * br_name){
 	char p[30]="/brokers/";
 	strcat(p,br_name);
 //	 char str[]="/xyz6";
@@ -122,6 +125,7 @@ void Zoo_cli::register_broker(char * br_name){
 	else{
 		cout<<"Register broker success"<<endl;
 	}
+	return rc;
 }
 /************Get Brokers*****
  * 获取当前系统所有在线的Broker
@@ -150,7 +154,7 @@ vector<string> Zoo_cli::get_brokers(){
  * 用于保存当前系统所有topic信息
  * 生产者创建topic时调用一次
  * ****/
-void Zoo_cli::register_topic(char *topic){
+int Zoo_cli::register_topic(char *topic){
 	char p[50]="/topics/";
 	strcat(p,topic);
 	int rc = zoo_create(zh,p,"subscribe",9,&ZOO_OPEN_ACL_UNSAFE,0,
@@ -158,12 +162,13 @@ void Zoo_cli::register_topic(char *topic){
 	if(rc){
 		std::cout<<"Register topic error!";
 	}
+	return rc;
 }
 /*******Delete Topic
  * 删除指定的topic
  * 如topic下有consumer,级联删除
  * ********************/
-void Zoo_cli::delete_topic(char *topic){
+int Zoo_cli::delete_topic(char *topic){
 	char p[50]="/topics/";
 	strcat(p,topic);
 	vector<string> result;
@@ -184,6 +189,7 @@ void Zoo_cli::delete_topic(char *topic){
 	if(rc){
 		std::cout<<"Delete topic error!"<<endl;
 	}
+	return rc;
 }
 /***********Get Topic
  * 获取当前所有topic
@@ -233,7 +239,7 @@ vector<string> Zoo_cli::get_topic_cosumers(char *topic){
 /***************Delete Topic consumer
  * 删除指定topic下的consumer节点
  * **************/
-void Zoo_cli::delete_topic_consumer(char * consumer,char * topic){
+int Zoo_cli::delete_topic_consumer(char * consumer,char * topic){
 	vector<string> consumers;
 	char p[50]="/topics/";
 	strcat(p,topic);
@@ -243,9 +249,10 @@ void Zoo_cli::delete_topic_consumer(char * consumer,char * topic){
 	if(rc){
 		std::cout<<"Delete topic consumer error!";
 	}
+	return rc;
 }
 /*************consumer***************/
-void Zoo_cli::register_consumer(char * con_name){
+int Zoo_cli::register_consumer(char * con_name){
 	//ZOO_EPHEMERAL 临时节点 ZOO_SEQUENCE 编号递增
 	//char *p ="/"+this->IP;
 	char p[30]="/consumers/";
@@ -258,14 +265,16 @@ void Zoo_cli::register_consumer(char * con_name){
 //	else{
 //		std::cout<<"consumer_id :"<<consumer_id;
 //	}
+	return rc;
 }
-void Zoo_cli::delete_consumer(char *consumer){
+int Zoo_cli::delete_consumer(char *consumer){
 	char p[50]="/consumers/";
 	strcat(p,consumer);
 	int rc = zoo_delete(zh,p,-1);
 	if(rc){
 		std::cout<<"Delete consumer error!";
 	}
+	return rc;
 }
 
 vector<string> Zoo_cli::get_consumers(){
@@ -289,7 +298,7 @@ vector<string> Zoo_cli::get_consumers(){
  * 在指定topic节点下创建consumer节点
  * 即某个consumer订阅某个topic
  * ************/
-void Zoo_cli::sub_(char * consumer,char* topic){
+int Zoo_cli::sub_(char * consumer,char* topic){
 	char p[50]="/topics/";
 	strcat(p,topic);
 	strcat(p,"/");
@@ -299,6 +308,7 @@ void Zoo_cli::sub_(char * consumer,char* topic){
 	if(rc){
 			std::cout<<"sub_ error! rc = "<<rc<<endl;
 		}
+	return rc;
 }
 
 
--- chat/zoo_cli.h
+++ chat/zoo_cli.h
@@ -22,22 +22,22 @@ public:
 	Zoo_cli(char *ip);//指定在zookeeper地址 默认127.0.0.1:2181
 	Zoo_cli();
 	~Zoo_cli();
-	void init();
+	int init();
 /***********broker***********************/
-	void register_broker(char *br_name);//初始化建立此brokerd的临时节点
+	int register_broker(char *br_name);//初始化建立此brokerd的临时节点
 	vector<string> get_brokers();//获取所有brokers列表
-	void register_topic(char *topic);//注册添加一个topic
-	void delete_topic(char *topic);
+	int register_topic(char *topic);//注册添加一个topic
+	int delete_topic(char *topic);
 	vector<string> get_topics();//获取所有topic列表
 	vector<string> get_topic_cosumers(char *topic);//获取某个topic下所有订阅者
 
-	//void pub(char* topic);	//发布
+	//int pub(char* topic);	//发布
 /************consumer*******************/
-	void register_consumer(char * consumer_id);
-	void delete_consumer(char *consumer);
+	int register_consumer(char * consumer_id);
+	int delete_consumer(char *consumer);
 	vector<string> get_consumers();//获取所有订阅者信息
-	void sub_(char * consumer,char* topic);	//consumer订阅topic
-	void delete_topic_consumer(char * consumer,char* topic);
+	int sub_(char * consumer,char* topic);	//consumer订阅topic
+	int delete_topic_consumer(char * consumer,char* topic);
 public:
 	char* IP="127.0.0.1:2181";
 	char* broker_id="1234";
